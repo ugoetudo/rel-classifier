efbb3940401f89a356b86927fc47a1481d44964e
PriyankarBose <bosep@vcu.edu>
2023-07-12 00:17:46 -0400

    New updates

--- src/App.js
+++ src/App.js
@@ -8,92 +8,121 @@ import './App.css';
 // import Backdrop from '@material-ui/core/Backdrop';
 // import Dialog from '@material-ui/core/Dialog';
 // import { useState } from "react";
+import DataFrame from "dataframe-js";
 import Records from "./records.json";
+import Xarrow from "react-xarrows";
+
+
+for (let i = 0; i < Records.length - 1; i++) {
+  if (Records[i].hasOwnProperty("spanid")) {
+    let token_text = Records[i].token_text;
+    let spanid = Records[i].spanid;
+    for (let j = i + 1; j < Records.length && Records[j].spanid === spanid; j++) {
+      token_text += " " + Records[j].token_text;
+      Records.splice(j, 1);
+      j--;
+    }
+    Records[i].token_text = token_text;
+  }
+};
+
+  const transformedRecord = [];
+  const spanidSet = new Set();
+  
+  Records.forEach(item => {
+    if (item.spanid && !spanidSet.has(item.spanid)) {
+      const joinedText = Records
+        .filter(i => i.spanid === item.spanid)
+        .map(i => i.token_text)
+        .join(' ');
+        
+      transformedRecord.push({
+        tkid: item.tkid,
+        token_text: joinedText,
+        spanid: item.spanid,
+        label_name: item.label_name
+      });
+      
+      spanidSet.add(item.spanid);
+    } else if (!item.spanid) {
+      transformedRecord.push(item);
+    }
+  });
+  
+  
+
+   
 
 class Relation extends React.Component {
+
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {
+    //     hovered: null,
+    //   };
+      
+    //   this.unsetHover = this.unsetHover.bind(this);
+    // };
+    
+    // setHover(id) {
+    //   return () => {
+    //     this.setState({ hovered: id });
+    //   }
+    // };
+    
+    // unsetHover() {
+    //   this.setState({ hovered: null });
+    // };
+
+    
+    render () {
+       const tokens_to_render = [];
+       const input_tokens = Records;
+
+       
+      
+      // console.log(spanid);
+       var cntr = 100;
   
-  state = {
-    color: 'lightblue',
-    hover: false
-  }
-
-  changeColor = () => {
-    console.log('Color Change with', 'green');
-    this.setState(prevState => {
-      return { color: prevState.color === 'lightblue'?'green':'lightblue',
-      hover: prevState.hover === false?true:false
-      }
-    })
-  }
-
-  // changeHover = () => {
-  //   this.setState(prevState => {
-  //     return { hover: prevState.hover === false?true:false
-  //     }
-  //   })
-  // }
-
-
-  render ()
-{
-  const tokens_to_render = [];
-  const input_tokens = Records;
-  
-  var cntr = 0;
-
-  input_tokens.forEach(tk => {
-    const span = tk['spanid'];
-    if (!tk['spanid'])
-    {
-        tokens_to_render.push(
-          <button key={cntr} 
-            index={tk['spanid']} style={{backgroundColor: 'lightblue'}}
-            // style={{backgroundColor: this.state.hover?this.state.color:'lightblue'}}  onMouseEnter={this.changeColor} onMouseLeave={ this.changeColor}
-          //   onSelected={(ix,value) => this.addToBundle(ix, value)}
-          > 
-              {tk['token_text']} 
-            </button>
-        )
-        cntr = cntr + 1;
+       input_tokens.forEach(tk => {
+           const span = tk['spanid'];
+           if (!tk['spanid']) {
+              // tokens_to_render.push();
+               tokens_to_render.push(
+                <div className = 'inner'>
+                   <div key={tk.tkid} >--</div>
+                   <div key={cntr} index={tk['spanid']} className='example_d'> 
+                       {tk['token_text']} 
+                  </div>
+                </div>
+              );
+              
+           } else {
+            // tokens_to_render.push();
+              tokens_to_render.push(
+                <div className = 'inner'>
+                  <div key={tk.tkid}>{tk['label_name']}</div>
+                  <div
+                    key={cntr}
+                    index={tk['spanid']}
+                    className='example_c' // ${tk.spanid === this.state.hovered && 'example_c-hovered'}`}
+                    // onMouseOver={this.setHover(tk.spanid)}
+                    // onMouseOut={this.unsetHover}
+                  > 
+                      {tk['token_text']} 
+                  </div>
+                </div>
+              );
+           };
+           cntr = cntr + 1;
+      });
+      console.log(Records);
+    
+      return (
+          <div key="id" className="control-box">
+             {tokens_to_render}
+          </div>
+      )
     }
-    else {
-      if(tk['spanid']===span){
-          tokens_to_render.push(
-            <button key={cntr} 
-              index={tk['spanid']} onMouseEnter={this.changeColor} onMouseLeave={ this.changeColor} style={{backgroundColor: this.state.hover?this.state.color:'lightblue'}}
-              // style={{backgroundColor: this.state.hover?this.state.color:'lightblue'}}  onMouseEnter={this.changeColor} onMouseLeave={ this.changeColor}
-            //   onSelected={(ix,value) => this.addToBundle(ix, value)}
-            > 
-                {tk['token_text']} 
-              </button>
-          )
-          cntr = cntr + 1;
-        }
-
-        else {
-          tokens_to_render.push(
-            <button key={cntr} 
-              index={tk['spanid']} style={{backgroundColor: 'lightblue'}}
-              // style={{backgroundColor: this.state.hover?this.state.color:'lightblue'}}  onMouseEnter={this.changeColor} onMouseLeave={ this.changeColor}
-            //   onSelected={(ix,value) => this.addToBundle(ix, value)}
-            > 
-                {tk['token_text']} 
-              </button>
-          )
-          cntr = cntr + 1;
-        }
-        }
-    }
-
-  );
-  
-  return(
-        <div className="control-box">
-          {tokens_to_render}
-        </div>
-  )
-
   }
-}
-
 export default Relation;
